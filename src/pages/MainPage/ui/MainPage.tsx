7bf402bd25e0e9839d7ae54028f4afda25a918ff
DmitryMS <dm.zhukov7@mail.ru>
2024-06-07 15:36:12 +0300

    NavHistoryPanel

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,10 @@
 import { ThemeSwitcher } from '@/features/ThemeSwitcher';
-import HistoryNav from '@/features/HistoryNav/ui/HistoryNav';
 import { Page } from '@/widgets/Page';
 
 const MainPage = () => {
 
     return (
         <Page id={'mainPage'}>
-            <HistoryNav/>
             <section className='section'>
                 <img height={100} width={620} src="/logo-big.svg" alt="petclinic big logo" />
                <ThemeSwitcher/>
--- src/widgets/HistoryNav/ui/HistoryNav.tsx
+++ src/widgets/HistoryNav/ui/HistoryNav.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Button } from "@/shared/ui/Button";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import cls from "./HistoryNav.module.scss";
 import { useAppSelector } from "@/shared/lib/hooks/useAppSelector/useAppSelector";
 import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
@@ -13,35 +13,25 @@ const HistoryNav = memo(() => {
   const historyPaths = useAppSelector((state) => state.historyMap) as any;
   const dispatch = useAppDispatch();
 
-  const onNavigationClick = () => {
-    dispatch(add({ name: "Pets", path: "http://localhost:3000/test" }));
-  };
-
-  const onRemoveClick = () => {
-    dispatch(removePath({ name: "Pets", path: "http://localhost:3000/test" }));
+  const onRemoveClick = (name:string,path:string) => {
+    dispatch(removePath({ name, path }));
   };
 
   const renderPaths = historyPaths.map(
     (item: { name: string; path: string }) => (
       <dt className={cls.linkItem}>
         <Link to={item.path}>{item.name}</Link>
-        <span className="SR-only" onClick={onRemoveClick}>
-          {" "}
-          X
-        </span>
+        <Button onClick={() => onRemoveClick(item.name,item.path)}>
+          x
+        </Button>
       </dt>
     )
   );
 
   return (
-    <>
       <nav>
         <dl className={cls.historyNav}>{renderPaths}</dl>
       </nav>
-      <Button type={"submit"} classes={[]} onClick={onNavigationClick}>
-        {"Войти"}
-      </Button>
-    </>
   );
 });
 
