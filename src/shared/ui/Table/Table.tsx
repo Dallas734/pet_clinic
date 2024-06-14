f102a2f63cb155853ce55dcb6d0dfa5ce2c4d06a
Mark <vernidub66@gmail.com>
2024-06-14 15:30:43 +0300

    Making getProperty function in Table

--- src/shared/ui/Table/Table.tsx
+++ src/shared/ui/Table/Table.tsx
@@ -3,22 +3,27 @@ import cls from "./Table.module.scss";
 import cnBind from "classnames/bind";
 import TableColumn from "./TableColumn";
 
-var sortMultidimensionalArrayFunc = <T extends Object>(inArr: Array<T>, inIndex: string, inOrderBy: string) => {
+var sortMultidimensionalArrayFunc = <T extends Object>(
+  inArr: Array<T>,
+  inIndex: string,
+  inOrderBy: string
+) => {
   var arr: Array<T> = inArr.length > 0 && inArr !== undefined ? inArr : [];
-  var index: string = inIndex.length > 1 && inIndex !== undefined ? inIndex : '';
+  var index: string =
+    inIndex.length > 1 && inIndex !== undefined ? inIndex : "";
   var orderBy = inOrderBy;
 
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     var classof = function classof(obj: Object) {
       return Object.prototype.toString.call(obj).slice(8, -1);
     };
 
-    if (classof(arr) !== 'Array') {
-      throw new Error('First argument must be a array');
+    if (classof(arr) !== "Array") {
+      throw new Error("First argument must be a array");
     }
 
-    if (classof(index) !== 'String') {
-      throw new Error('Second argument must be a string');
+    if (classof(index) !== "String") {
+      throw new Error("Second argument must be a string");
     }
   }
 
@@ -43,12 +48,34 @@ var sortMultidimensionalArrayFunc = <T extends Object>(inArr: Array<T>, inIndex:
   }
 
   switch (orderBy) {
-    case 'asc':
+    case "asc":
       return arr.sort(asc);
-    case 'desc':
+    case "desc":
       return arr.sort(desc);
     default:
       return arr;
+  }
+};
+
+const getProperty = (indexes: String[], i: number, el: Object) => {
+  type P = keyof typeof el;
+  let newProperty;
+  //console.log(Object.getPrototypeOf(el));
+  if (Object.getPrototypeOf(el) !== String) {
+    newProperty = el[indexes[i] as P];
+    // console.log(newProperty);
+    if (indexes[i + 1] !== undefined && newProperty) {
+      i++;
+      getProperty(indexes, i, newProperty);
+    }
+  } else {
+    console.log(newProperty);
+    //console.log(indexes);
+    //console.log(el[indexes[i - 1] as P])
+    return newProperty;
+    // if (Object.getPrototypeOf(el) === String) {
+    //   return newProperty;
+    // } else return el[indexes[i] as P];
   }
 };
 
@@ -56,7 +83,7 @@ interface TableProps<T> {
   classes?: string[];
   head: TableColumn[];
   data?: Array<T>;
-  setHead?: (arr: TableColumn[]) => void; 
+  setHead?: (arr: TableColumn[]) => void;
 }
 
 export const Table = <T extends Object>(props: TableProps<T>) => {
@@ -68,32 +95,35 @@ export const Table = <T extends Object>(props: TableProps<T>) => {
     setViewData(data);
   }, [data]);
 
-  const sortTableFunc = (id: string, sortMethod: string, index: number) => {  
-  
+  const sortTableFunc = (id: string, sortMethod: string, index: number) => {
     let currentSortMethod = sortMethod;
     switch (sortMethod) {
-      case 'none':
-        currentSortMethod = 'none'
+      case "none":
+        currentSortMethod = "none";
         break;
-      case 'default':
-        currentSortMethod = 'asc';
+      case "default":
+        currentSortMethod = "asc";
         break;
-      case 'asc':
-        currentSortMethod = 'desc';
+      case "asc":
+        currentSortMethod = "desc";
         break;
-      case 'desc':
-        currentSortMethod = 'asc';
+      case "desc":
+        currentSortMethod = "asc";
         break;
       default:
-        currentSortMethod = 'asc';
+        currentSortMethod = "asc";
     }
 
-    head.forEach(x => x.index === id ? x.sortMethod = currentSortMethod : x.sortMethod);
+    head.forEach((x) =>
+      x.index === id ? (x.sortMethod = currentSortMethod) : x.sortMethod
+    );
     if (setHead) setHead(head);
-    
+
     let arrayForSort = [...viewData];
-    setViewData(sortMultidimensionalArrayFunc(arrayForSort, id, currentSortMethod));
-  }
+    setViewData(
+      sortMultidimensionalArrayFunc(arrayForSort, id, currentSortMethod)
+    );
+  };
 
   classes.push("Table");
 
@@ -106,7 +136,17 @@ export const Table = <T extends Object>(props: TableProps<T>) => {
           <thead>
             <tr>
               {head.map((el, index) => (
-                <th className={cls.sorting} onClick={() => {sortTableFunc(head[index].index, head[index].sortMethod, index)}} key={el.index}>
+                <th
+                  className={cls.sorting}
+                  onClick={() => {
+                    sortTableFunc(
+                      head[index].index,
+                      head[index].sortMethod,
+                      index
+                    );
+                  }}
+                  key={el.index}
+                >
                   {el.name}
                 </th>
               ))}
@@ -116,10 +156,11 @@ export const Table = <T extends Object>(props: TableProps<T>) => {
             {viewData.map((el: T, index) => (
               <tr key={index}>
                 {head.map((column) => {
-                  type P = keyof typeof el;
-                  const property = el[column.index as P];
+                  const indexes = column.index.split(".");
+                  const property = getProperty(indexes, 0, el);
+                  console.log(property);
                   return (
-                    <td key={column.index}>
+                    <td key={indexes[0]}>
                       <>{property}</>
                     </td>
                   );
