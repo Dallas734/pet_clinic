f6c87d380cf7707917eef00e1cbbc5e15c078bf1
Artem-Ky <artoad505@mail.ru>
2024-06-14 10:08:53 +0300

    feat: mini calendar

--- src/pages/VisitPage/ui/VisitPage.tsx
+++ src/pages/VisitPage/ui/VisitPage.tsx
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import cls from "./VisitPage.module.scss";
-
-import classNames from "classnames";
-import { Calendar } from "@/widgets/Calendars/CalendarMini/Calendar";
+import { PageSwapper } from "@/widgets/PageSwapper";
+import CalendarVisitPageMode from "./PageMode/Calendar/CalendarVisitPageMode";
 
 const PetsPage: React.FC = () => {
+  const Option = ['Календарь', 'Все посещения'] as string[];
+  const [currentOption, setCurrentOprion] = useState(Option[0]);
+
+
   return (
     <>
       <h2 className="sr-only">visit calendar</h2>
-      <section className={cls.containerOption} style={{ width: 300 }}>
-        <div className={cls.option}>
-          <h3 className={cls.title}></h3>
-          <Calendar />
-        </div>
-        <div className={cls.option}>
-          <h3 className={cls.title}></h3>
-        </div>
-        <div className={cls.option}>
-          <h3 className={cls.title}></h3>
-        </div>
-      </section>
-      <section></section>
+      <PageSwapper PageOptions={Option} currentOption={currentOption} setCurrentOption={setCurrentOprion}/>
+      <div className={cls.content}>
+        {currentOption === Option[0] && <CalendarVisitPageMode/>}
+        {currentOption === Option[1] && <>тут будет другой режим</>}
+      </div>
     </>
   );
 };
